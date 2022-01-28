6c4be50ae8f53653a72190c68e6a733a2f2b1d25
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-28 21:14:04 +0700

    card
    import 'package:flutter/material.dart';
    
    void main() {
      runApp(MaterialApp(
        title: "Sidafa Aplication",
        home: HalamanSatu(),
      ));
    }
    
    class HalamanSatu extends StatelessWidget {
      @override
      Widget build(BuildContext context) {
        return new Scaffold(
          appBar: new AppBar(
            title: const Text("card & Parsing"),
          ),
          body: new Container(
            child: new Column(
              crossAxisAlignment: CrossAxisAlignment.stretch,
              children: <Widget>[
                new Card(
                  child: new Column(
                    children: <Widget>[
                      new Icon(
                        Icons.home,
                        size: 50.0,
                        color: Colors.brown,
                      ),
                      new Text(
                        "home",
                        style: new TextStyle(fontSize: 20.0),
                      )
                    ],
                  ),
                ),
                new Card(
                  child: new Column(
                    children: <Widget>[
                      new Icon(
                        Icons.home,
                        size: 50.0,
                        color: Colors.brown,
                      ),
                      new Text(
                        "home",
                        style: new TextStyle(fontSize: 20.0),
                      )
                    ],
                  ),
                ),
              ],
            ),
          ),
        );
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,47 @@ void main() {
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          leading: const Icon(Icons.home),
-          title: const Center(
-            child: Text("SIDAFA"),
-          ),
-          actions: const <Widget>[Icon(Icons.search)],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text("card & Parsing"),
+      ),
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Card(
+              child: new Column(
+                children: <Widget>[
+                  new Icon(
+                    Icons.home,
+                    size: 50.0,
+                    color: Colors.brown,
+                  ),
+                  new Text(
+                    "home",
+                    style: new TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+            new Card(
+              child: new Column(
+                children: <Widget>[
+                  new Icon(
+                    Icons.home,
+                    size: 50.0,
+                    color: Colors.brown,
+                  ),
+                  new Text(
+                    "home",
+                    style: new TextStyle(fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
-        body: Column(children: <Widget>[
-          const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
-          const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
-          Row(
-            children: const <Widget>[
-              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
-              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
-              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
-            ],
-          ),
-          const Icon(Icons.cake, size: 70.0, color: Colors.red),
-          const Icon(Icons.cake, size: 70.0, color: Colors.red),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const <Widget>[
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const <Widget>[
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const <Widget>[
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-              Icon(Icons.favorite, size: 70.0, color: Colors.red),
-            ],
-          ),
-          Row(
-            children: const <Widget>[
-              const Icon(Icons.add, size: 70.0, color: Colors.blue)
-            ],
-          )
-        ]));
+      ),
+    );
   }
 }
