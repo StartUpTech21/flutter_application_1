e2c0b5f85addaa3c8e28a94dcc07e6596007c07e
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-28 20:38:51 +0700

    layout-row&column
    import 'package:flutter/material.dart';
    
    void main() {
      runApp(MaterialApp(
        title: "Sidafa Aplication",
        home: HalamanSatu(),
      ));
    }
    
    class HalamanSatu extends StatelessWidget {
      @override
      Widget build(BuildContext context) {
        return Scaffold(
            backgroundColor: Colors.grey[300],
            appBar: AppBar(
              backgroundColor: Colors.blue,
              leading: const Icon(Icons.home),
              title: const Center(
                child: Text("SIDAFA"),
              ),
              actions: const <Widget>[Icon(Icons.search)],
            ),
            body: Column(children: <Widget>[
              const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
              const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
              Row(
                children: const <Widget>[
                  Icon(Icons.donut_large, size: 70.0, color: Colors.red),
                  Icon(Icons.donut_large, size: 70.0, color: Colors.red),
                  Icon(Icons.donut_large, size: 70.0, color: Colors.red),
                ],
              ),
              const Icon(Icons.cake, size: 70.0, color: Colors.red),
              const Icon(Icons.cake, size: 70.0, color: Colors.red),
              Row(
                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                // mainAxisAlignment: MainAxisAlignment.spaceAround,
                children: const <Widget>[
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                ],
              ),
              Row(
                mainAxisAlignment: MainAxisAlignment.spaceAround,
                children: const <Widget>[
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                ],
              ),
              Row(
                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                // mainAxisAlignment: MainAxisAlignment.spaceAround,
                children: const <Widget>[
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                  Icon(Icons.favorite, size: 70.0, color: Colors.red),
                ],
              ),
              Row(
                children: const <Widget>[
                  const Icon(Icons.add, size: 70.0, color: Colors.blue)
                ],
              )
            ]));
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     title: "Sidafa Aplication",
-    home: new HalamanSatu(),
+    home: HalamanSatu(),
   ));
 }
 
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: new AppBar(
-        backgroundColor: Colors.blue,
-        leading: new Icon(Icons.home),
-        title: new Center(
-          child: new Text("SIDAFA"),
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: const Icon(Icons.home),
+          title: const Center(
+            child: Text("SIDAFA"),
+          ),
+          actions: const <Widget>[Icon(Icons.search)],
         ),
-        actions: <Widget>[new Icon(Icons.search)],
-      ),
-      body: new Center(
-        child: new Text(
-          "Selamat datang di aplikasi SIDAFA",
-          style: new TextStyle(
-              color: Colors.black, fontFamily: "Serif", fontSize: 30.0),
-        ),
-      ),
-    );
+        body: Column(children: <Widget>[
+          const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
+          const Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
+          Row(
+            children: const <Widget>[
+              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
+              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
+              Icon(Icons.donut_large, size: 70.0, color: Colors.red),
+            ],
+          ),
+          const Icon(Icons.cake, size: 70.0, color: Colors.red),
+          const Icon(Icons.cake, size: 70.0, color: Colors.red),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const <Widget>[
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const <Widget>[
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const <Widget>[
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+              Icon(Icons.favorite, size: 70.0, color: Colors.red),
+            ],
+          ),
+          Row(
+            children: const <Widget>[
+              const Icon(Icons.add, size: 70.0, color: Colors.blue)
+            ],
+          )
+        ]));
   }
 }
