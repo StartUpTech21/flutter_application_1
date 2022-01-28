8066ea1fa0e7cfd60a1959cf8bd9706fa67e2e01
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-28 17:09:26 +0700

    Sidafa tampilan coba coba
    import 'package:flutter/material.dart';
    
    void main() {
      runApp(new MaterialApp(
        title: "Sidafa Aplication",
        home: new HalamanSatu(),
      ));
    }
    
    class HalamanSatu extends StatelessWidget {
      @override
      Widget build(BuildContext context) {
        return new Scaffold(
          backgroundColor: Colors.grey[300],
          appBar: new AppBar(
            backgroundColor: Colors.blue,
            leading: new Icon(Icons.home),
            title: new Center(
              child: new Text("SIDAFA"),
            ),
            actions: <Widget>[new Icon(Icons.search)],
          ),
          body: new Center(
            child: new Text(
              "Selamat datang di aplikasi SIDAFA",
              style: new TextStyle(
                  color: Colors.black, fontFamily: "Serif", fontSize: 30.0),
            ),
          ),
        );
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,21 @@ class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.yellow[100],
+      backgroundColor: Colors.grey[300],
       appBar: new AppBar(
-        backgroundColor: Colors.red[200],
+        backgroundColor: Colors.blue,
         leading: new Icon(Icons.home),
         title: new Center(
-          child: new Text("Sidafa"),
+          child: new Text("SIDAFA"),
         ),
         actions: <Widget>[new Icon(Icons.search)],
+      ),
+      body: new Center(
+        child: new Text(
+          "Selamat datang di aplikasi SIDAFA",
+          style: new TextStyle(
+              color: Colors.black, fontFamily: "Serif", fontSize: 30.0),
+        ),
       ),
     );
   }
