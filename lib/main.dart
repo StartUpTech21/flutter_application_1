d53a8df5feed841c26526cd614b14627354fad90
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-29 23:43:42 +0700

    AppBar Default

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    title: "SIDAFA Aplication",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.blue,
+        leading: new Icon(Icons.home),
+        title: new Center(
+          child: new Text("SIDAFA"),
+        ),
+      ),
+    );
+  }
+}
