eb0034ab1af1328f57e4a968e3df2d88bf8324fb
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-01-30 21:31:12 +0700

    ListView dengan array
    import 'package:flutter/material.dart';
    
    void main() {
      runApp(MaterialApp(
        title: "SIDAFA Aplication",
        home: new Home(
          data: new List<String>.generate(300, (i) => "ini data ke $i"),
        ),
      ));
    }
    
    class Home extends StatelessWidget {
      // const Home({Key? key}) : super(key: key);
    
      final List<String> data;
      Home({required this.data});
    
      @override
      Widget build(BuildContext context) {
        return new Scaffold(
          appBar: new AppBar(
            backgroundColor: Colors.blue,
            leading: new Icon(Icons.home),
            title: new Center(
              child: new Text("SIDAFA-List View"),
            ),
          ),
          body: new Container(
            child: new ListView.builder(
                itemCount: data.length,
                itemBuilder: (context, index) {
                  return new ListTile(
                    leading: new Icon(Icons.widgets),
                    title: new Text("${data[index]}"),
                  );
                }),
          ),
        );
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "SIDAFA Aplication",
-    home: new Home(),
+    home: new Home(
+      data: new List<String>.generate(300, (i) => "ini data ke $i"),
+    ),
   ));
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  // const Home({Key? key}) : super(key: key);
+
+  final List<String> data;
+  Home({required this.data});
 
   @override
   Widget build(BuildContext context) {
@@ -20,57 +25,16 @@ class Home extends StatelessWidget {
           child: new Text("SIDAFA-List View"),
         ),
       ),
-      body: new ListView(
-        padding: EdgeInsets.all(20.0),
-        children: <Widget>[
-          new ListViewData(
-              gambar: "images/user.png", username: "user1", IdSantri: "01"),
-          new ListViewData(
-              gambar: "images/user.png", username: "user1", IdSantri: "01"),
-          new ListViewData(
-              gambar: "images/user.png", username: "user1", IdSantri: "01"),
-        ],
+      body: new Container(
+        child: new ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return new ListTile(
+                leading: new Icon(Icons.widgets),
+                title: new Text("${data[index]}"),
+              );
+            }),
       ),
     );
-  }
-}
-
-class ListViewData extends StatelessWidget {
-  var gambar;
-  var username;
-  var IdSantri;
-  ListViewData({this.gambar, this.username, this.IdSantri});
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-        padding: EdgeInsets.all(20.0),
-        child: new Center(
-          child: new Row(
-            children: <Widget>[
-              new Image.asset(
-                gambar,
-                width: 150.0,
-              ),
-              new Container(
-                padding: EdgeInsets.all(10.0),
-                child: new Center(
-                  child: new Column(
-                    children: <Widget>[
-                      new Text(
-                        IdSantri,
-                        style: new TextStyle(fontSize: 20.0),
-                      ),
-                      new Text(
-                        username,
-                        style: new TextStyle(fontSize: 20.0),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
   }
 }
