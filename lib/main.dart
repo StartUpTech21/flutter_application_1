73d0762e5fa7c27f5a2dbcd70f1f42fef48f0105
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-02-01 06:52:43 +0700

    ListView JSON-error HTML
    import 'package:flutter/material.dart';
    import 'package:http/http.dart' as http;
    import 'dart:async';
    import 'dart:convert';
    
    void main() {
      runApp(MaterialApp(
        title: "SIDAFA Aplication",
        home: new Home(),
      ));
    }
    
    class Home extends StatefulWidget {
      const Home({Key? key}) : super(key: key);
    
      @override
      _HomeState createState() => _HomeState();
    }
    
    class _HomeState extends State<Home> {
      late List dataJSON;
    
      var JSON;
    
      Future<String?> ambildata() async {
        http.Response hasil = await http.get(
          Uri.parse("https://jsonplaceholder.typicode.com/posts"),
          headers: {"Accept": "application/json"},
        );
    
        this.setState(() {
          dataJSON = JSON.decode(hasil.body);
        });
    
        return "Success!";
      }
    
      @override
      void initState() {
        this.ambildata();
      }
    
      @override
      Widget build(BuildContext context) {
        return new Scaffold(
          appBar: new AppBar(
            title: new Text("List Data JSON"),
          ),
          body: new ListView.builder(
            itemCount: dataJSON == null ? 0 : dataJSON.length,
            itemBuilder: (context, i) {
              return new Card(
                child: new Text(dataJSON[i]['title']),
              );
            },
          ),
        );
      }
    }

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
     title: "SIDAFA Aplication",
-    home: new Home(
-      data: new List<String>.generate(300, (i) => "ini data ke $i"),
-    ),
+    home: new Home(),
   ));
 }
 
-class Home extends StatelessWidget {
-  // const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
-  final List<String> data;
-  Home({required this.data});
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List dataJSON;
+
+  var JSON;
+
+  Future<String?> ambildata() async {
+    http.Response hasil = await http.get(
+      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+      headers: {"Accept": "application/json"},
+    );
+
+    this.setState(() {
+      dataJSON = JSON.decode(hasil.body);
+    });
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    this.ambildata();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor: Colors.blue,
-        leading: new Icon(Icons.home),
-        title: new Center(
-          child: new Text("SIDAFA-List View"),
-        ),
+        title: new Text("List Data JSON"),
       ),
-      body: new Container(
-        child: new ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return new ListTile(
-                leading: new Icon(Icons.widgets),
-                title: new Text("${data[index]}"),
-              );
-            }),
+      body: new ListView.builder(
+        itemCount: dataJSON == null ? 0 : dataJSON.length,
+        itemBuilder: (context, i) {
+          return new Card(
+            child: new Text(dataJSON[i]['title']),
+          );
+        },
       ),
     );
   }
